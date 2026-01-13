5b599a575ab7b55c5cfdb05532a834cf3317da10
salim user <abouobeidasalim@gmail.com>
2026-01-13 11:31:23 +0100

    Trigger pipeline

--- api/app.py
+++ api/app.py
@@ -1,43 +1,43 @@
 from flask import Flask, request
 import hashlib
 import subprocess
-import os
 
 app = Flask(__name__)
 
+# Mot de passe en dur (mauvaise pratique)
+ADMIN_PASSWORD = "123456"
 
-ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "default_secure_value_change_me")
-
+# Cryptographie faible (MD5)
 def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
+    return hashlib.md5(password.encode()).hexdigest()
 
 @app.route("/login")
 def login():
     username = request.args.get("username")
     password = request.args.get("password")
 
-    if not username or not password:
-        return "Missing credentials", 400
-
+    # Authentification faible
     if username == "admin" and hash_password(password) == hash_password(ADMIN_PASSWORD):
         return "Logged in"
-    return "Invalid credentials", 401
+    return "Invalid credentials"
 
 @app.route("/ping")
 def ping():
     host = request.args.get("host", "localhost")
+
+    # Injection de commande (shell=True)
     result = subprocess.check_output(
         f"ping -c 1 {host}",
         shell=True
     )
     return result
+
 @app.route("/hello")
 def hello():
     name = request.args.get("name", "user")
-    
+    # XSS potentiel
     return f"<h1>Hello {name}</h1>"
 
-
 if __name__ == "__main__":
-    
+    # Debug activé
     app.run(debug=True)
